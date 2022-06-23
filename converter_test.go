2dee0fd843ebddf2bf9684453a1407c293574319
icemint0828 <icemint0828@gmail.com>
2022-06-23 12:46:52 +0900

    add test case

--- converter_test.go
+++ converter_test.go
@@ -334,6 +334,11 @@ func Test_converter_Filter(t *testing.T) {
 			fields: fields{Image: GetPngImage()},
 			args:   args{SepiaModel},
 		},
+		{
+			name:   "alpha",
+			fields: fields{Image: GetAlphaImage()},
+			args:   args{SepiaModel},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
