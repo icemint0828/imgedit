139fe5aee98290839d373137c694f9c02b7bea3a
icemint0828 <icemint0828@gmail.com>
2022-06-23 09:53:40 +0900

    added the feature to lay down images

--- converter.go
+++ converter.go
@@ -33,6 +33,7 @@ type Converter interface {
 	ReverseY()
 	Grayscale()
 	AddString(text string, options *StringOptions)
+	Tile(xLength, yLength int)
 	Convert() image.Image
 }
 
@@ -270,6 +271,21 @@ func drawString(dst draw.Image, drawer *font.Drawer, outlineDrawer *font.Drawer,
 
 		drawer.DrawBytes([]byte{s})
 	}
+}
+
+func (c *converter) Tile(xLength, yLength int) {
+	dst := image.NewRGBA(image.Rect(0, 0, c.Bounds().Dx()*xLength, c.Bounds().Dy()*yLength))
+	srcSize := c.Bounds().Size()
+	for xLen := 0; xLen < xLength; xLen++ {
+		for yLen := 0; yLen < yLength; yLen++ {
+			for x := 0; x < srcSize.X; x++ {
+				for y := 0; y < srcSize.Y; y++ {
+					dst.Set(x+srcSize.X*xLen, y+srcSize.Y*yLen, c.Image.At(x, y))
+				}
+			}
+		}
+	}
+	c.Image = dst
 }
 
 // Convert get convert image
